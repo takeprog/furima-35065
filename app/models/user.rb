bc46b822f90a075589bdd97ad4b04217ea77901e
takeprog <take.prog@gmail.com>
2021-04-04 12:20:12 +0900

    レビュー指摘箇所訂正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,29 +4,16 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  
-  validates :nickname, presence: true
-  validates :last_name, presence: true
-  validates :first_name, presence: true
-  validates :kana_last_name, presence: true
-  validates :kana_first_name, presence: true
-  validates :birthday, presence: true
-
-  
-  kanji = /\A[ぁ-んァ-ヶ一-龥々ー]+\z/                        #全角かな、カナ、漢字のみ
-  validates :last_name, format: { with: kanji }
-  validates :first_name, format: { with: kanji }
-
-  kana = /\A[ァ-ヶー－]+\z/                        #全角カナのみ
-  validates :kana_last_name, format: { with: kana }
-  validates :kana_first_name, format: { with: kana }
+  with_options presence: true do
+    validates :nickname
+    validates :last_name, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/} #全角かな、カナ、漢字のみ
+    validates :first_name, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/} 
+    validates :kana_last_name, format: {with: /\A[ァ-ヶー]+\z/} #全角カナのみ
+    validates :kana_first_name, format: {with: /\A[ァ-ヶー]+\z/}
+    validates :birthday, format: { with: /\A\d{4}-\d{2}-\d{2}\z/ } #2019-10-04"のような形でのみ
+  end
 
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください' 
-
-  year_month_day = /\A\d{4}-\d{2}-\d{2}\z/      #2019-10-04"のような形でのみ
-  validates :birthday, format: { with: year_month_day }
-
-  
 
 end
