4572663ee194bc950d1f1ae0fdfa66f5d577c364
takeprog <take.prog@gmail.com>
2021-04-07 21:46:23 +0900

    商品情報編集レビュー指摘事項改善

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
-
+  before_action :set_item, only: [:edit, :show, :update]
   before_action :authenticate_user!, only: [:new, :create, :edit, :update]
-  before_action :move_to_index, only: :edit
+  before_action :move_to_index, only: [:edit, :update]
 
   def index
     @items=Item.all.order("created_at DESC")
@@ -21,15 +21,12 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item=Item.find(params[:id])
   end
 
   def edit
-    @item=Item.find(params[:id])
   end
 
   def update
-    @item=Item.find(params[:id])
     if @item.update(item_params)
       redirect_to  root_path
     else
@@ -47,6 +44,10 @@ class ItemsController < ApplicationController
     unless Item.find(params[:id]).user_id == current_user.id
       redirect_to action: :index
     end
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 
 end
